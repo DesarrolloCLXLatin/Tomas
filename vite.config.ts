16e4bc3ae69740fee3005c8eba9a02ee95d38172
david.c <david.c@aisd.io>
2025-05-14 07:15:44 -0400

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['e6dc09d8f60789.lhr.life'],
+  },
   optimizeDeps: {
     exclude: [''],
   },
